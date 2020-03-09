09cd12a07b66bb1e1b1a9e77ab00e9b0815d19ba
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-03-09 12:31:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state = {
+      task: '',
+      id: Date.now(),
+      isCompleted: false
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
